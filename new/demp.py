42b4ccda883ed1f406eb041dfd3ec75b36c8ffce
Sam <zhangyiluo@126.com>
2019-12-09 10:21:01 +0800

    Create demp.py
    最简单的访问链接

---
+++ new/demp.py
@@ -0,0 +1,18 @@
+#coding=utf-8
+import urllib.request
+
+# This code test in python3.8 pass.
+# Attention! urllib is diffenent from low version urllib2
+def getData():
+    url = 'https://example.com'
+    headers = {
+        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+    }
+    req = urllib.request.Request(url, headers=headers)
+    html = urllib.request.urlopen(req).read()
+    print(html)
+
+def main():
+    datalist=getData()
+ 
+main()
