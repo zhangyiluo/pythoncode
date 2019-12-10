75d381fd7a670ef4eccabe36624e6cfb463d2edc
Sam <zhangyiluo@126.com>
2019-12-10 09:39:00 +0800

    Update demp.py

--- new/demp.py
+++ new/demp.py
@@ -9,8 +9,9 @@ def getData():
         'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
     }
     req = urllib.request.Request(url, headers=headers)
-    html = urllib.request.urlopen(req).read()
-    print(html)
+    print(type(req))
+    #html = urllib.request.urlopen(req).read()
+    #print(html)
 
 def main():
     datalist=getData()
